e24101903ef681741f377b1b10ddade08d1083fe
RSlysz <remi@unity3d.com>
2019-05-15 15:32:44 +0200

    Add inspector for Environement and EnvironmentLibrary

--- com.unity.render-pipelines.core/Editor/LookDev/Environment.cs
+++ com.unity.render-pipelines.core/Editor/LookDev/Environment.cs
@@ -2,53 +2,48 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using System;
 using System.Collections.Generic;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
 
 namespace UnityEditor.Rendering.LookDev
 {
-    //[CreateAssetMenu(fileName = "Environment", menuName = "LookDev/Environment", order = 1)]
+    [CreateAssetMenu(fileName = "Environment", menuName = "LookDev/Environment", order = 1)]
     public class Environment : ScriptableObject
     {
+        //[TODO: check if the shadow/sky split worth the indirection]
+        //Note: multi-edition is not supported as we cannot draw multiple HDRI
         [Serializable]
         public class Shadow
         {
-            public Cubemap cubemap; //[TODO: check]
+            public Cubemap cubemap;
             // Setup default position to be on the sun in the default HDRI.
             // This is important as the defaultHDRI don't call the set brightest spot function on first call.
-            public float angleOffset = 0.0f; //[TODO: sync with sky]
-            //public SphericalHarmonicsL2 shadowAmbientProbe; //[TODO: check interest for shadow]
             [SerializeField]
             private float m_Latitude = 60.0f; // [-90..90]
             [SerializeField]
             private float m_Longitude = 299.0f; // [0..360]
             [field: SerializeField]
-            private float intensity { get; set; } = 1.0f;
+            public float intensity { get; set; } = 1.0f;
             [field: SerializeField]
             public Color color { get; set; } = Color.white;
 
             public float latitude
             {
                 get => m_Latitude;
-                set { m_Latitude = value; ConformLatLong(); }
+                //[TODO: check why it was originally clamped to 89 instead of 90]
+                set => m_Latitude = Mathf.Clamp(value, -90, 90);
             }
 
             public float longitude
             {
-                get => m_Longitude;
-                set { m_Longitude = value; ConformLatLong(); }
-            }
-
-            private void ConformLatLong()
-            {
-                // Clamp latitude to [-90..90]
-                if (m_Latitude < -90.0f)
-                    m_Latitude = -90.0f;
-                if (m_Latitude > 89.0f)
-                    m_Latitude = 89.0f;
-
-                // wrap longitude around
-                m_Longitude = m_Longitude % 360.0f;
-                if (m_Longitude < 0.0)
-                    m_Longitude = 360.0f + m_Longitude;
+                get => m_Longitude; 
+                set
+                {
+                    // Clamp longitude to [0..360]
+                    m_Longitude = value % 360f;
+                    if (m_Longitude < 0.0)
+                        m_Longitude = 360f + m_Longitude;
+                }
             }
         }
 
@@ -57,96 +52,234 @@ namespace UnityEditor.Rendering.LookDev
         {
             public Cubemap cubemap;
             public float angleOffset = 0.0f;
-            public SphericalHarmonicsL2 ambientProbe;
         }
 
         public Sky sky = new Sky();
         public Shadow shadow = new Shadow();
     }
 
-    //[CreateAssetMenu(fileName = "EnvironmentLibrary", menuName = "LookDev/EnvironmentLibrary", order = 1)]
-    public class EnvironmentLibrary : ScriptableObject
-    {
-        [field: SerializeField]
-        List<Environment> environments { get; set; } = new List<Environment>();
-
-        public int Count => environments.Count;
-        public Environment this[int index] => environments[index];
-
-        public void Add()
-        {
-            Environment environment = ScriptableObject.CreateInstance<Environment>();
-            Undo.RegisterCreatedObjectUndo(environment, "Add Environment");
-
-            // Store this new environment as a subasset so we can reference it safely afterwards.
-            AssetDatabase.AddObjectToAsset(environment, this);
-
-            environments.Add(environment);
-
-            // Force save / refresh. Important to do this last because SaveAssets can cause effect to become null!
-            EditorUtility.SetDirty(this);
-            AssetDatabase.SaveAssets();
-        }
-
-        public void Remove(int index)
-        {
-            Environment environment = environments[index];
-            Undo.RecordObject(this, "Remove Environment");
-            environments.RemoveAt(index);
-            Undo.DestroyObjectImmediate(environment);
-
-            // Force save / refresh
-            EditorUtility.SetDirty(this);
-            AssetDatabase.SaveAssets();
-        }
-    }
-
     [CustomEditor(typeof(Environment))]
     class EnvironmentEditor : Editor
     {
-        override public Texture2D RenderStaticPreview(string assetPath, UnityEngine.Object[] subAssets, int width, int height)
-        {
-            height = height >> 1; //quick *.5
-            Environment environment = (target as Environment);
+        EnvironmentElement m_EnvironmentElement;
 
+        public sealed override VisualElement CreateInspectorGUI()
+            => m_EnvironmentElement = new EnvironmentElement(target as Environment);
+
+        // Don't use ImGUI
+        public sealed override void OnInspectorGUI() { }
+
+        override public Texture2D RenderStaticPreview(string assetPath, UnityEngine.Object[] subAssets, int width, int height)
+            => EnvironmentElement.GetLatLongThumbnailTexture(target as Environment, width);
+    }
+
+    interface IBendable<T>
+    {
+        void Assign(T data);
+    }
+    public class EnvironmentElement : VisualElement, IBendable<Environment>
+    {
+        const int k_SkyThumbnailWidth = 200;
+        const int k_SkyThumbnailHeight = 100;
+        const int k_SkadowThumbnailWidth = 60;
+        const int k_SkadowThumbnailHeight = 30;
+        const int k_SkadowThumbnailXPosition = 130;
+        const int k_SkadowThumbnailYPosition = 10;
+        static Material s_cubeToLatlongMaterial;
+        static Material cubeToLatlongMaterial
+            => s_cubeToLatlongMaterial ?? (s_cubeToLatlongMaterial = new Material(Shader.Find("Hidden/LookDev/CubeToLatlong")));
+        
+        VisualElement environmentParams;
+        Environment environment;
+
+        Image latlong;
+        ObjectField skyCubemapField;
+        Slider skyRotationOffset;
+        ObjectField shadowCubemapField;
+        FloatField shadowLatitude;
+        FloatField shadowLongitude;
+        FloatField shadowIntensity;
+        ColorField shadowColor;
+
+        public EnvironmentElement() => Create();
+
+        public EnvironmentElement(Environment environment)
+        {
+            Create();
+            Assign(environment);
+        }
+
+        void Create()
+        {
+            environmentParams = new VisualElement();
+
+            latlong = new Image();
+            latlong.style.width = k_SkyThumbnailWidth;
+            latlong.style.height = k_SkyThumbnailHeight;
+            Add(latlong);
+
+            Foldout skyFoldout = new Foldout()
+            {
+                text = "Sky"
+            };
+            skyCubemapField = new ObjectField("Cubemap");
+            skyCubemapField.allowSceneObjects = false;
+            skyCubemapField.objectType = typeof(Cubemap);
+            skyCubemapField.RegisterValueChangedCallback(evt =>
+            {
+                environment.sky.cubemap = evt.newValue as Cubemap;
+                latlong.image = GetLatLongThumbnailTexture();
+                EditorUtility.SetDirty(environment);
+            });
+            skyFoldout.Add(skyCubemapField);
+
+            skyRotationOffset = new Slider("Angle Offset", 0f, 360f);
+            skyRotationOffset.RegisterValueChangedCallback(evt =>
+            {
+                environment.sky.angleOffset = evt.newValue;
+                latlong.image = GetLatLongThumbnailTexture();
+                EditorUtility.SetDirty(environment);
+            });
+            skyFoldout.Add(skyRotationOffset);
+            var style = skyFoldout.Q<Toggle>().style;
+            style.marginLeft = 3;
+            style.unityFontStyleAndWeight = FontStyle.Bold;
+            environmentParams.Add(skyFoldout);
+
+            Foldout shadowFoldout = new Foldout()
+            {
+                text = "Shadow"
+            };
+            shadowCubemapField = new ObjectField("Cubemap");
+            shadowCubemapField.allowSceneObjects = false;
+            shadowCubemapField.objectType = typeof(Cubemap);
+            shadowCubemapField.RegisterValueChangedCallback(evt =>
+            {
+                environment.shadow.cubemap = evt.newValue as Cubemap;
+                latlong.image = GetLatLongThumbnailTexture();
+                EditorUtility.SetDirty(environment);
+            });
+            shadowFoldout.Add(shadowCubemapField);
+
+            shadowLatitude = new FloatField("Latitude");
+            shadowLatitude.RegisterValueChangedCallback(evt =>
+            {
+                environment.shadow.latitude = evt.newValue;
+                //clamping code occurred. Reassign clamped value
+                shadowLatitude.SetValueWithoutNotify(environment.shadow.latitude);
+                EditorUtility.SetDirty(environment); //
+            });
+            shadowFoldout.Add(shadowLatitude);
+
+            shadowLongitude = new FloatField("Longitude");
+            shadowLongitude.RegisterValueChangedCallback(evt =>
+            {
+                environment.shadow.longitude = evt.newValue;
+                //clamping code occurred. Reassign clamped value
+                shadowLongitude.SetValueWithoutNotify(environment.shadow.longitude);
+                EditorUtility.SetDirty(environment);
+            });
+            shadowFoldout.Add(shadowLongitude);
+
+            shadowIntensity = new FloatField("Intensity");
+            shadowIntensity.RegisterValueChangedCallback(evt =>
+            {
+                environment.shadow.intensity = evt.newValue;
+                EditorUtility.SetDirty(environment);
+            });
+            shadowFoldout.Add(shadowIntensity);
+
+            shadowColor = new ColorField("Color");
+            shadowColor.RegisterValueChangedCallback(evt =>
+            {
+                environment.shadow.color = evt.newValue;
+                EditorUtility.SetDirty(environment);
+            });
+            shadowFoldout.Add(shadowColor);
+            style = shadowFoldout.Q<Toggle>().style;
+            style.marginLeft = 3;
+            style.unityFontStyleAndWeight = FontStyle.Bold;
+            environmentParams.Add(shadowFoldout);
+            Add(environmentParams);
+        }
+
+        public void Assign(Environment environment)
+        {
+            this.environment = environment;
+            if (environment == null || environment.Equals(null))
+                return;
+
+            latlong.image = GetLatLongThumbnailTexture();
+            skyCubemapField.SetValueWithoutNotify(environment.sky.cubemap);
+            skyRotationOffset.SetValueWithoutNotify(environment.sky.angleOffset);
+            shadowCubemapField.SetValueWithoutNotify(environment.shadow.cubemap);
+            shadowLatitude.SetValueWithoutNotify(environment.shadow.latitude);
+            shadowLongitude.SetValueWithoutNotify(environment.shadow.longitude);
+            shadowIntensity.SetValueWithoutNotify(environment.shadow.intensity);
+            shadowColor.SetValueWithoutNotify(environment.shadow.color);
+        }
+
+        public Texture2D GetLatLongThumbnailTexture()
+            => GetLatLongThumbnailTexture(environment, k_SkyThumbnailWidth);
+
+        public static Texture2D GetLatLongThumbnailTexture(Environment environment, int width)
+        {
+            int height = width >> 1;
             RenderTexture oldActive = RenderTexture.active;
-            //[TODO: optimize RenderTexture creation]
-            RenderTexture temporaryRT = new RenderTexture(width, height, 0);
+            RenderTexture temporaryRT = new RenderTexture(width, height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.sRGB);
             RenderTexture.active = temporaryRT;
-            EnvironmentUtil.cubeToLatlongMaterial.SetTexture("_MainTex", environment.sky.cubemap);
-            EnvironmentUtil.cubeToLatlongMaterial.SetVector("_WindowParams", new Vector4(height, -1000.0f, 2, 1.0f)); // Doesn't matter but let's match DrawLatLongThumbnail settings,-1000.0f to be sure to not have clipping issue (we should not clip normally but don't want to create a new shader)
-            EnvironmentUtil.cubeToLatlongMaterial.SetVector("_CubeToLatLongParams", new Vector4(Mathf.Deg2Rad * environment.sky.angleOffset, 0.5f, 1.0f, 0.0f));
-            EnvironmentUtil.cubeToLatlongMaterial.SetPass(0);
+            cubeToLatlongMaterial.SetTexture("_MainTex", environment.sky.cubemap);
+            cubeToLatlongMaterial.SetVector("_WindowParams",
+                new Vector4(
+                    height, //height
+                    -1000f, //y position, -1000f to be sure to not have clipping issue (we should not clip normally but don't want to create a new shader)
+                    2f,      //margin value
+                    1f));   //Pixel per Point
+            cubeToLatlongMaterial.SetVector("_CubeToLatLongParams",
+                new Vector4(
+                    Mathf.Deg2Rad * environment.sky.angleOffset,    //rotation of the environment in radian
+                    1f,     //alpha
+                    1f,     //intensity
+                    0f));   //LOD
+            cubeToLatlongMaterial.SetPass(0);
             GL.LoadPixelMatrix(0, width, height, 0);
-            GL.Clear(true, true, default);
-            Renderer.DrawFullScreenQuad(new Rect(0, 0, width, height));
+            GL.Clear(true, true, Color.black);
+            Rect skyRect = new Rect(0, 0, width, height);
+            Renderer.DrawFullScreenQuad(skyRect);
+
+            if (environment.shadow.cubemap != null)
+            {
+                cubeToLatlongMaterial.SetTexture("_MainTex", environment.shadow.cubemap);
+                cubeToLatlongMaterial.SetVector("_WindowParams",
+                    new Vector4(
+                        height, //height
+                        -1000f, //y position, -1000f to be sure to not have clipping issue (we should not clip normally but don't want to create a new shader)
+                        2f,      //margin value
+                        1f));   //Pixel per Point
+                cubeToLatlongMaterial.SetVector("_CubeToLatLongParams",
+                    new Vector4(
+                        Mathf.Deg2Rad * environment.sky.angleOffset,    //rotation of the environment in radian
+                        1f,   //alpha
+                        0.3f,   //intensity
+                        0f));   //LOD
+                cubeToLatlongMaterial.SetPass(0);
+                int shadowWidth = (int)(width * (k_SkadowThumbnailWidth / (float)k_SkyThumbnailWidth));
+                int shadowXPosition = (int)(width * (k_SkadowThumbnailXPosition / (float)k_SkyThumbnailWidth));
+                int shadowYPosition = (int)(width * (k_SkadowThumbnailYPosition / (float)k_SkyThumbnailWidth));
+                Rect shadowRect = new Rect(
+                    shadowXPosition,
+                    shadowYPosition,
+                    shadowWidth,
+                    shadowWidth >> 1);
+                Renderer.DrawFullScreenQuad(shadowRect);
+            }
+
             Texture2D result = new Texture2D(width, height, TextureFormat.ARGB32, false);
             result.ReadPixels(new Rect(0, 0, width, height), 0, 0, false);
             result.Apply(false);
             RenderTexture.active = oldActive;
-            DestroyImmediate(temporaryRT);
+            UnityEngine.Object.DestroyImmediate(temporaryRT);
             return result;
         }
-    }
-
-    [CustomEditor(typeof(EnvironmentLibrary))]
-    class EnvironmentLibraryEditor : Editor
-    {
-
-        // We don't want users to edit these in the inspector
-        public override void OnInspectorGUI()
-        {
-            //DrawDefaultInspector();
-            //if (GUILayout.Button("Add"))
-            //{
-            //    (target as EnvironmentLibrary).Add();
-            //}
-        }
-    }
-
-    class EnvironmentUtil
-    {
-        static public Material cubeToLatlongMaterial = new Material(Shader.Find("Hidden/LookDev/CubeToLatlong"));
-        static public Material cubemapMaterial = new Material(Shader.Find("Skybox/Cubemap"));
     }
 }
