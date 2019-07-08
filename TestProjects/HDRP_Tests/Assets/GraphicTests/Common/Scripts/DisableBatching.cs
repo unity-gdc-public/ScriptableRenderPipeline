f54a49c4550cef4b0d5df74cf145deb441976ada
TomasKiniulis <tomas.kiniulis@unity3d.com>
2019-07-08 12:11:28 +0300

    Srp batcher automated scenes

---
+++ TestProjects/HDRP_Tests/Assets/GraphicTests/Common/Scripts/DisableBatching.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DisableBatching : MonoBehaviour
+{
+    private void Start()
+    {
+        ToggleBatching();
+    }
+    public void ToggleBatching()
+    {
+        Invoke("Batch", 0.1f); //function is invoked due to batching toggle issue not getting executed on start
+    }
+
+    private void Batch()
+    {
+        GraphicsSettings.useScriptableRenderPipelineBatching = !GraphicsSettings.useScriptableRenderPipelineBatching;
+    }
+}
