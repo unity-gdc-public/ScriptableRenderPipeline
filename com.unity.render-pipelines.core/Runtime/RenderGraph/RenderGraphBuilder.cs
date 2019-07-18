e6e55c9234ed16167e89c02c928437ea2874d3ea
Julien Ignace <julien@unity3d.com>
2019-07-18 17:42:55 +0200

    Scripting API for Render Graph

--- com.unity.render-pipelines.core/Runtime/RenderGraph/RenderGraphBuilder.cs
+++ com.unity.render-pipelines.core/Runtime/RenderGraph/RenderGraphBuilder.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace UnityEngine.Experimental.Rendering.RenderGraphModule
 {
+    /// <summary>
+    /// The RenderGraphBuilder is the class that allows users to setup a new Render Pass
+    /// </summary>
     public struct RenderGraphBuilder : IDisposable
     {
         RenderGraph.RenderPass      m_RenderPass;
@@ -10,6 +13,13 @@ namespace UnityEngine.Experimental.Rendering.RenderGraphModule
         bool                        m_Disposed;
 
         #region Public Interface
+        /// <summary>
+        /// Specify that a texture resource will be used as a color render target during the pass
+        /// This will have the same effect as WriteTexture and will also automatically set the texture as a render target
+        /// </summary>
+        /// <param name="input">Texture resource that will be used as a color render target</param>
+        /// <param name="index">Index for multiple render target usage</param>
+        /// <returns>An updated resource handle to the input resource</returns>
         public RenderGraphMutableResource UseColorBuffer(in RenderGraphMutableResource input, int index)
         {
             if (input.type != RenderGraphResourceType.Texture)
@@ -20,6 +30,12 @@ namespace UnityEngine.Experimental.Rendering.RenderGraphModule
             return input;
         }
 
+        /// <summary>
+        /// Specify that a texture resource will be used a depth buffer during the pass
+        /// </summary>
+        /// <param name="input">Texture resource that will be used as a depth buffer</param>
+        /// <param name="flags">Specify if the depth buffer will be read, written to, or both</param>
+        /// <returns>An updated resource handle to the input resource</returns>
         public RenderGraphMutableResource UseDepthBuffer(in RenderGraphMutableResource input, DepthAccess flags)
         {
             if (input.type != RenderGraphResourceType.Texture)
@@ -33,6 +49,11 @@ namespace UnityEngine.Experimental.Rendering.RenderGraphModule
             return input;
         }
 
+        /// <summary>
+        /// Specify that a texture resource will be read during the pass
+        /// </summary>
+        /// <param name="input">Texture resource that will be read during the pass</param>
+        /// <returns>An updated resource handle to the input resource</returns>
         public RenderGraphResource ReadTexture(in RenderGraphResource input)
         {
             if (input.type != RenderGraphResourceType.Texture)
@@ -42,6 +63,11 @@ namespace UnityEngine.Experimental.Rendering.RenderGraphModule
             return input;
         }
 
+        /// <summary>
+        /// Specify that a texture resource will be written to during the pass
+        /// </summary>
+        /// <param name="input">Texture resource that will be written to during the pass</param>
+        /// <returns>An updated resource handle to the input resource</returns>
         public RenderGraphMutableResource WriteTexture(in RenderGraphMutableResource input)
         {
             if (input.type != RenderGraphResourceType.Texture)
@@ -52,23 +78,42 @@ namespace UnityEngine.Experimental.Rendering.RenderGraphModule
             return input;
         }
 
-        public RenderGraphResource UseRendererList(in RenderGraphResource resource)
+        /// <summary>
+        /// Specify that a renderer list resource will be used during this pass
+        /// </summary>
+        /// <param name="input">Renderer List resource that will be used during the pass</param>
+        /// <returns>An updated resource handle to the input resource</returns>
+        public RenderGraphResource UseRendererList(in RenderGraphResource input)
         {
-            if (resource.type != RenderGraphResourceType.RendererList)
+            if (input.type != RenderGraphResourceType.RendererList)
                 throw new ArgumentException("Trying use a resource that is not a renderer list.");
-            m_RenderPass.usedRendererListList.Add(resource);
-            return resource;
+            m_RenderPass.usedRendererListList.Add(input);
+            return input;
         }
+
+        /// <summary>
+        /// Specify the render function used for this pass
+        /// A call to this is mandatory for the pass to be valid
+        /// </summary>
+        /// <typeparam name="PassData">Type of the class used to provide data to the Render Pass</typeparam>
+        /// <param name="renderFunc">Render function for the pass</param>
         public void SetRenderFunc<PassData>(RenderFunc<PassData> renderFunc) where PassData : class, new()
         {
             ((RenderGraph.RenderPass<PassData>)m_RenderPass).renderFunc = renderFunc;
         }
 
+        /// <summary>
+        /// Enable asynchronous compute for this pass
+        /// </summary>
+        /// <param name="value">Set to true to enable asynchronous compute</param>
         public void EnableAsyncCompute(bool value)
         {
             m_RenderPass.enableAsyncCompute = value;
         }
 
+        /// <summary>
+        /// Dispose the RenderGraphBuilder instance
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
